93693ff161e40c733749edad28c423611e893488
Kristina Mouzakitis <mouzakik@mcmaster.ca>
2024-11-22 13:22:56 -0500

    Iteration 2A

--- objPosArrayList_TestSuite/objPos.h
+++ objPosArrayList_TestSuite/objPos.h
@@ -18,7 +18,9 @@ class objPos
         objPos(int xPos, int yPos, char sym);
         
         // Respect the rule of six / minimum four
-        // [TODO] Implement the missing special member functions to meet the minimum four rule
+        objPos(const objPos &p);
+        objPos& operator=(const objPos &p);
+        ~objPos();        
         
         void setObjPos(objPos o);        
         void setObjPos(int xPos, int yPos, char sym);  
